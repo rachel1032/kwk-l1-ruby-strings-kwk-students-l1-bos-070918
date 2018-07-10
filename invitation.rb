9340e2cdddc58cbe4c9dab2d32c62b7c50a85bab
rachel1032 <racheles32@gmail.com>
2018-07-10 14:23:52 +0000

    automatically backed up by learn

--- invitation.rb
+++ invitation.rb
@@ -1,24 +1,4 @@
-# * guest_name
-# * party_name
-# * date
-# * time
-# * host_name
 
-# ...and then prints out custom invitations that look something like this:
-
-# ```
-# Dear Hermione,
-
-# You are cordially invited to the Best Halloween Party Ever on October 31 at 6pm. Please RSVP no later than October 30.
-
-# Sincerely,
-
-# Harry Potter
-# ```
-
-
-
-# Code your prompts here!
 
 puts "Who is the party guest?"
 
